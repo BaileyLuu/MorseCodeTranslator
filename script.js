702e72f16bad5cda6ccd525af21188449e40cf9a
Bailey Luu <50964948+BaileyLuu@users.noreply.github.com>
2021-10-12 05:24:18 +0000

    Add translator and decryption

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ function translator(plainText){
     document.getElementById("statement").innerHTML =  "\"" + plainText + "\" in Morse Code is..."
     document.getElementById("morseCode").innerHTML = text;
 }
+function decryption(morseCode){
+   const morseCode2 = [
+  //A       B      C       D      E     F     G
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." , ".----", "..---", "...--", "....-", "....", "-....", "--...", "---..", "----.", "-----"]
+
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    morseCode += " ";
+    var MCode = "";
+    var finalText = "";
+    for(var i = 0; i < morseCode.length; i++){
+      if(morseCode[i] != " " ){
+        MCode += morseCode[i];
+      }else{
+        MCode += "";
+        for(var j = 0; j < morseCode2.length; j++){
+          if(MCode == morseCode2[j]){
+            finalText += alphabet[j];
+            MCode = "";
+          }
+        }
+      }
+      
+    }
+document.getElementById("decrypt").innerHTML = finalText;
+}
